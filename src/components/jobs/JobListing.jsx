5bfc66277e7d6f07025df9acad11e1ab9599b1c9
Sagar Moriya <sagarmaurya814@gmail.com>
2024-05-02 13:21:14 +0530

    integrated loader

--- src/components/jobs/JobListing.jsx
+++ src/components/jobs/JobListing.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchJobs } from "../../features/jobs/jobSlice";
 import { Container, Grid } from "@mui/material";
+
+import { fetchJobs } from "../../features/jobs/jobSlice";
 import JobItem from "./JobItem";
+import Loader from "../ui/Loader";
 
 const JobListing = () => {
   const dispatch = useDispatch();
@@ -12,18 +14,21 @@ const JobListing = () => {
     dispatch(fetchJobs({ limit: jobSize, offset: 0 }));
   }, []);
 
-  if (!loading && !error)
-    return (
-      <Container>
-        <Grid container spacing={3}>
-          {jobs.map((jobData) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={jobData.jdUid}>
-              <JobItem {...jobData} />
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-    );
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <Container>
+      <Grid container spacing={3}>
+        {jobs.map((jobData) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={jobData.jdUid}>
+            <JobItem {...jobData} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default JobListing;
---
+++ src/components/ui/Loader.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+const Loader = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
+
+export default Loader;
